a0a37f716366d4fc63d97a2bd1602e30810b14b9
Shrikant Tambe <tambe.shrikant29@gmail.com>
2019-05-10 00:10:42 +0530

    Add files via upload

---
+++ First_Program.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  9 23:21:46 2019
+
+@author: sunbeam
+"""
+
+a = 1
+print(a)
+
+a = a + 1
+print(a)
+
+a = a + 1
+print(a)
+
+a = a + 1
+print(a)
+
+a = a + 1
+print(a)
+
+a = a + 1
+print(a)
+
+a = a + 1
+print(a)
+
+a = a + 1
+print(a)
+
+a = a + 1
+print(a)
+
+a = a + 1; print(a)
